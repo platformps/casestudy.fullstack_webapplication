6129b9ffbd00e542498394dc6af6053637a69131
CJ Fulton <fulchr3356@gmail.com>
2020-07-06 15:46:50 -0400

    Created Spring/React Project

---
+++ src/test/java/io/fulchr3356/upkeepassistant/UpkeepAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fulchr3356.upkeepassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpkeepAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
