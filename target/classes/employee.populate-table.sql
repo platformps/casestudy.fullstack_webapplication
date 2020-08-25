575e1bf6b7f4332eb9c2adb855b228412b4325ab
CJ Fulton <fulchr3356@gmail.com>
2020-08-25 15:41:53 -0400

    Removing all files in .gitignore

--- target/classes/employee.populate-table.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO employee ( department, email,first_name,last_name,password,position,salary) VALUES ('sales', 'fulchr3356@gmail.com','Christopher','Fulton','password','Manager',100000.00);
