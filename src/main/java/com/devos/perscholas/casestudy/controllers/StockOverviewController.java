70ffa43db14ef08596b2ef1ae74d4ed24667de23
nick-devos <nickdevos123@yahoo.com>
2020-09-13 15:53:21 -0400

    StockOverview gets JSON data

---
+++ src/main/java/com/devos/perscholas/casestudy/controllers/StockOverviewController.java
@@ -0,0 +1,23 @@
+package com.devos.perscholas.casestudy.controllers;
+
+import com.devos.perscholas.casestudy.models.StockOverview;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/stocks")
+public class StockOverviewController {
+    @GetMapping("/OVERVIEW/{symbol}")
+    public StockOverview getCompanyOverview(@PathVariable("symbol") String symbol) {
+        StockOverview stock = new RestTemplate().getForObject(constructApiCall(symbol), StockOverview.class);
+        return stock;
+    }
+
+    private String constructApiCall(String symbol) {
+        String apiCall = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + symbol + "&apikey=7IEE375T5KRNN35W";
+        return apiCall;
+    }
+}
