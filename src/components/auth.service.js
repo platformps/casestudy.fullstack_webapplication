cc084626bf968eb6d71f9e79e71846197340ffe8
CJ Fulton <fulchr3356@gmail.com>
2020-09-20 20:13:34 -0400

    changed url again

--- src/components/auth.service.js
+++ src/components/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://upkeep-assistant.herokuapp.com/api/auth/";
+const API_URL = "https://upkeep-assistant.herokuapp.com/api/auth/";
 
 class AuthService {
   login(username, password) {
