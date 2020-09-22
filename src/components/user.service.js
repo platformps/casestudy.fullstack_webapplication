52a539fac17c0a094e0d1bdcbe793bdee62eaf54
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2020-09-22 13:46:06 -0400

    Update user.service.js

--- src/components/user.service.js
+++ src/components/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "https://upkeep-assistant.herokuapp.com/api/auth/";
 class UserService {
   getPublicContent() {
     return axios.get(API_URL + 'all');
