2d8cbee55a76b0637660a9a3cecb3b23f4056918
CJ Fulton <fulchr3356@gmail.com>
2020-09-22 11:04:59 -0400

    Auto stash before merge of "master" and "origin/master"

--- src/components/auth.service.js
+++ src/components/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://upkeep-assistant.herokuapp.com/api/auth/";
+const API_URL = "http://localhost:8080/api/auth/";
 
 class AuthService {
   login(username, password) {
@@ -31,7 +31,7 @@ class AuthService {
   }
 
   getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse(localStorage.getItem("user"));
   }
 }
 
--- src/components/user.service.js
+++ src/components/user.service.js
@@ -1,24 +1,12 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'https://upkeep-assistant.herokuapp.com/api/test/';
-
+const API_URL = "http://localhost:8080/api/auth/";
 class UserService {
   getPublicContent() {
     return axios.get(API_URL + 'all');
   }
-
-  getUserBoard() {
-    return axios.get(API_URL + 'user', { headers: authHeader() });
-  }
-
-  getModeratorBoard() {
-    return axios.get(API_URL + 'mod', { headers: authHeader() });
-  }
-
-  getAdminBoard() {
-    return axios.get(API_URL + 'admin', { headers: authHeader() });
-  }
 }
+  
 
 export default new UserService();
