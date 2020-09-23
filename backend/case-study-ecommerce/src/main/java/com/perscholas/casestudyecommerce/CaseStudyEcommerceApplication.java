1691380d8b79f68a45e09cf8bca5a7e09fcc4321
razeroual <47251928+razeroual@users.noreply.github.com>
2020-09-23 10:24:01 -0400

    start

---
+++ backend/case-study-ecommerce/src/main/java/com/perscholas/casestudyecommerce/CaseStudyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.perscholas.casestudyecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyEcommerceApplication.class, args);
+	}
+
+}
---
+++ backend/case-study-ecommerce/src/test/java/com/perscholas/casestudyecommerce/CaseStudyEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perscholas.casestudyecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
