9a0fd6c326ebb15a0f4931bf7bbdca474fdf6ad7
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2020-09-23 23:35:28 -0400

    Update EmployeeList.js

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -22,7 +22,7 @@ class EmployeeList extends Component {
 
   componentDidMount() {
     this.setState({isLoading: true});
-    fetch('http://upkeep-assistant.herokuapp.com/api/employee',{headers: authHeader() })
+    fetch('https://upkeep-assistant.herokuapp.com/api/employee',{headers: authHeader() })
       .then(response => response.json())
       .then(data => this.setState({employee: data, isLoading: false}))
   }
